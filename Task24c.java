2ee1c9b957fd1006764feb4883dfcda4076d1ab1
annastakanova <50991493+annastakanova@users.noreply.github.com>
2019-09-25 13:22:04 +0300

    Update Task24c.java

--- Task24c.java
+++ Task24c.java
@@ -1,13 +1,11 @@
+package com.epam;
 //Составить программу, определяющую результат гадания на ромашке — «любит—не любит»,
 // взяв за исходное данное количество лепестков п.
-
-package com.epam;
 
 public class Task24c {
     public static void main(String[] args) {
         int n = 25;
         if (n%2 ==0) System.out.println("не любит :(");
         else System.out.println("любит :)");
-
     }
 }
