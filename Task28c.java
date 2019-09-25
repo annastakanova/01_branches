682c613d7bd8ff08b4781cfd640c1ccd6a7caf95
annastakanova <50991493+annastakanova@users.noreply.github.com>
2019-09-25 13:26:17 +0300

    Update Task28c.java

--- Task28c.java
+++ Task28c.java
@@ -1,7 +1,6 @@
-//Даны три числа a, b, с. Определить, какое из них равно d.
-// Если ни одно не равно d, то найти max(d — a, d — b, d —c).
-
 package com.epam;
+//Даны три числа a, b, с. Определить, какое из них равно d.
+// Если ни одно не равно d, то найти max(d — a, d — b, d —c).
 
 public class Task28c {
     public static void main(String[] args) {
