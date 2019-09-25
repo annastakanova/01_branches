b532dbb215912de42b0c29dd464fafa869596f62
annastakanova <50991493+annastakanova@users.noreply.github.com>
2019-09-25 13:30:22 +0300

    Update Task39c.java

--- Task39c.java
+++ Task39c.java
@@ -1,9 +1,7 @@
+package com.epam;
 //Вычислить значение функции:
 // f(x) = -x*x + x - 9, если x>=8
 // f(x) = 1/(Math.pow(x, 4) - 6), если x<8
-
-
-package com.epam;
 
 public class Task39c {
     public static void main(String[] args) {
