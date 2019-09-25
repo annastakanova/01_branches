ac110fdf2760290437b644f18d9a7ef318f7537a
annastakanova <50991493+annastakanova@users.noreply.github.com>
2019-09-25 13:12:34 +0300

    Update Task02c.java

--- Task02c.java
+++ Task02c.java
@@ -1,6 +1,6 @@
+package com.epam;
 //Составить программу сравнения двух чисел 1 и 2.
 // Если 1 меньше 2 – вывести на экран слово «yes», в противном случае – слово «no»
-package com.epam;
 
 public class Task02c {
     public static void main(String[] args) {
